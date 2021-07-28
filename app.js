2ae54be1a74f620aa1001c84fc8ae2eec98c708b
Serena <serenaqkong@gmail.com>
2021-07-28 16:48:58 -0700

    finished post/delete/get item routes and added error finding

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const { NotFoundError } = require("./expressError");
 const shoppingListRoutes = require("./shoppingListRoutes");
 
 const app = express();
 
 app.use(express.json());
 app.use("/items", shoppingListRoutes);
+
+/** 404 handler: matches unmatched routes; raises NotFoundError. */
+app.use(function (req, res, next) {
+  return next(new NotFoundError());
+});
+
+/** Error handler: logs stacktrace and returns JSON error message. */
+app.use(function (err, req, res, next) {
+  const status = err.status || 500;
+  const message = err.message;
+  if (process.env.NODE_ENV !== "test") console.error(status, err.stack);
+  return res.status(status).json({ error: { message, status } });
+});
+
 module.exports = app;
--- shoppingListRoutes.js
+++ shoppingListRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
-
+const { route } = require("./app");
+const { NotFoundError, BadRequestError } = require("./expressError")
 const db = require("./fakeDb");
 const router = new express.Router();
 
@@ -10,8 +11,43 @@ router.get("/", function (req, res, next) {
 
 /* POST /items: accepts JSON body, add item, and return it */
 router.post("/", function (req, res, next) {
+  if (!req.body.name || !req.body.price) {
+    throw new BadRequestError();
+  } else if (db.items.find(item => req.body.name === item.name)) {
+    // to prevent adding duplicate items to list
+    throw new BadRequestError("Error: item already exists.")
+  }
   db.items.push({ name: req.body.name, price: req.body.price });
   return res.json({ added: { name: req.body.name, price: req.body.price } });
 });
+
+/* GET /items/:name: return single item */
+router.get("/:name", function (req, res, next) {
+  let item = db.items.find(item => req.params.name === item.name);
+  if (!item) {
+    throw new NotFoundError(`${req.params.name} not found`);
+  } 
+  return res.json(item);
+})
+
+/* PATCH /items/:name:  accept JSON body, modify item, return it */
+router.patch("/:name", function (req, res, next) {
+  let itemIdx = db.items.findIndex(item => req.params.name === item.name);
+  if (itemIdx === -1) {
+    throw new NotFoundError(`${req.params.name} not found`);
+  }
+  db.items[itemIdx] = { name: req.body.name, price: req.body.price }
+  return res.json({updated: { name: req.body.name, price: req.body.price }});
+})
+
+/* DELETE /items/:name: delete item */
+router.delete("/:name", function (req, res, next) {
+  let itemIdx = db.items.findIndex(item => req.params.name === item.name);
+  if (itemIdx === -1) {
+    throw new NotFoundError(`${req.params.name} not found`);
+  }
+  db.items.splice(itemIdx, 1);
+  return res.json({message: "Deleted"})
+})
 
 module.exports = router;
